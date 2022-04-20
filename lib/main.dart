86577be9c574c8160107a90e3e2808a985925fe3
Aryan <hiaryan02@gmail.com>
2022-04-20 18:11:00 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Diceee'),
           backgroundColor: Colors.white,
         ),
         body: DicePage(),
